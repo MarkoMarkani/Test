17aeb595999b7c650a8c79368e7913f572ec60d6
MarkoMarkani <42622031+MarkoMarkani@users.noreply.github.com>
2020-09-08 09:15:44 +0200

    Cleaned up a bit, removed unnecessary files

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,41 +3,46 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const Navbar = () => {
-  const links = (
-    <ul>
-      <li>
-        <Link to='/'>Entities</Link>
-      </li>
-      <li>
-        <Link to='/'>Topics</Link>
-      </li>
-      <li>
-        <Link to='/'>
-          <i className='fas fa-user' />{' '}
-          <span className='hide-sm'>Dashboard</span>
-        </Link>
-      </li>
-    </ul>
-  );
-
+  const toggleScreen = (e) => {
+   console.log("Hello amigos");
+  };
 
   return (
-    <nav className='navbar bg-dark'>
-      <h1>
-        <Link to='/'>
-          <i className='fas fa-link' /> Connexions
-        </Link>
-      </h1>
-      <Fragment>{links}</Fragment>
-    </nav>
+    <Fragment>
+      <div className='d-flex' id='wrapper'>
+        <div className='bg-dark border-right' id='sidebar-wrapper'>
+          <div className='sidebar-heading'> Select action </div>
+          <div className='list-group list-group-flush'>
+            <Link to='#' className='list-group-item list-group-item-action bg-dark'>
+              Rules overview
+            </Link>
+            <Link to='#' className='list-group-item list-group-item-action bg-dark'>
+              Rules history
+            </Link>
+            <Link to='#' className='list-group-item list-group-item-action bg-dark'>
+              Topics
+            </Link>
+            <Link to='#' className='list-group-item list-group-item-action bg-dark'>
+              Maps
+            </Link>
+            <Link to='#' className='list-group-item list-group-item-action bg-dark'>
+              Status
+            </Link>
+          </div>
+        </div>
+        <div id='page-content-wrapper'>
+          <nav className='navbar navbar-expand-lg navbar-dark bg-dark border-bottom'>
+            <i className='navbar-toggler-icon' id='menu-toggle' onClick={toggleScreen}></i>
+            <h2 className='title'> Connexions </h2>
+          </nav>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
-
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
-export default connect(
-  mapStateToProps
-)(Navbar);
+export default connect(mapStateToProps)(Navbar);
